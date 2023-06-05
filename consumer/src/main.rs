668c107360de2d7e40e9a12724fa7de487fd6d0d
jdecroock <decroockjovi@gmail.com>
2023-06-05 17:44:12 +0200

    damn it works

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -1,16 +1,37 @@
-// DEFER example
-// query SlowAndFastFieldWithDefer {
-//    ... on Query @defer {
-//        slowField
-//      }
-//      fastField
-//    }
+use std::error::Error;
 
-// STREAM example
-// query StreamAlphabet {
-//  alphabet @stream
-//}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = reqwest::Client::new();
+    let body = r#"{
+        "query": "query { fastField ... on Query @defer { slowField } }"
+    }"#;
 
-fn main() {
-    println!("Hello, world!");
+    let mut resp = client.post("http://localhost:4000/graphql")
+        .body(body)
+        .header("Accept", "multipart/mixed")
+        .header("Content-Type", "application/json")
+        .send()
+        .await?;
+
+    while let Some(chunk) = resp.chunk().await? {
+        println!("Chunk: {:?}", chunk);
+    }
+
+    let body = r#"{
+        "query": "query { alphabet @stream }"
+    }"#;
+
+    let mut resp = client.post("http://localhost:4000/graphql")
+        .body(body)
+        .header("Accept", "multipart/mixed")
+        .header("Content-Type", "application/json")
+        .send()
+        .await?;
+
+    while let Some(chunk) = resp.chunk().await? {
+        println!("Chunk: {:?}", chunk);
+    }
+
+    Ok(())
 }
