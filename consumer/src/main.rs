1defedb224fb5166a17dd2b8a5804f02959d7a73
jdecroock <decroockjovi@gmail.com>
2023-06-05 17:46:29 +0200

    text/event-stream

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -33,5 +33,35 @@ async fn main() -> Result<(), Box<dyn Error>> {
         println!("Chunk: {:?}", chunk);
     }
 
+    let body = r#"{
+        "query": "query { fastField ... on Query @defer { slowField } }"
+    }"#;
+
+    let mut resp = client.post("http://localhost:4000/graphql")
+        .body(body)
+        .header("Accept", "text/event-stream")
+        .header("Content-Type", "application/json")
+        .send()
+        .await?;
+
+    while let Some(chunk) = resp.chunk().await? {
+        println!("Chunk: {:?}", chunk);
+    }
+
+    let body = r#"{
+        "query": "query { alphabet @stream }"
+    }"#;
+
+    let mut resp = client.post("http://localhost:4000/graphql")
+        .body(body)
+        .header("Accept", "text/event-stream")
+        .header("Content-Type", "application/json")
+        .send()
+        .await?;
+
+    while let Some(chunk) = resp.chunk().await? {
+        println!("Chunk: {:?}", chunk);
+    }
+
     Ok(())
 }
