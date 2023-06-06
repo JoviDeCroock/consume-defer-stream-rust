8de5bb33a0a2acdac158cfe1e6a72d5671b6e554
jdecroock <decroockjovi@gmail.com>
2023-06-06 17:12:06 +0200

    omg it works

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -1,6 +1,7 @@
-use std::{error::Error, collections::HashMap};
-use serde::{Deserialize,Deserializer};
+use std::{error::Error, fmt::format};
+use serde::{Deserialize};
 use serde_json::{Value};
+use json_dotpath::{DotPaths};
 
 #[derive(Debug, Deserialize)]
 #[serde(untagged)]
@@ -10,29 +11,53 @@ enum GraphQLResult {
 }
 
 #[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
 struct ExecutionResult {
     data: Value,
     // TODO: error
-    hasNext: bool,
+    has_next: bool,
 }
 
+
 impl ExecutionResult {
+    fn finalize(&mut self) -> &mut Self {
+        self.has_next = false;
+        self
+    }
+
     fn merge(&mut self, streamed_result: StreamedExecutionResult) -> &mut Self {
         streamed_result.incremental.iter().for_each(|incremental_payload| {
             match incremental_payload {
                 IncrementalPayload::DeferPayload(payload) => {
-                    if payload.path.len() == 0 {
+                    if payload.path.is_empty() {
                         let deferred_data = payload.data.as_object().unwrap();
-                        deferred_data.keys().for_each(|key| {
-                            let value = deferred_data.get(key).expect("Key to be present");
-                            self.data.as_object().unwrap().insert(key.to_owned(), value.to_owned());
-                        });
-                    } else {
 
+                        if let Value::Object(obj) = &self.data {
+                            let mut execution_data = obj.clone();
+                            deferred_data.keys().for_each(|key| {
+                                let value = deferred_data.get(key).expect("Key to be present");
+                                execution_data.insert(key.to_owned(), value.to_owned());
+                            });
+                            self.data = Value::Object(execution_data);
+                        } 
+                    } else {
+                        // TODO: nested case
                     }
                 }
                 IncrementalPayload::StreamPayload(payload) => {
-                    
+                    if let Value::Object(obj) = &self.data {
+                        let mut execution_data = obj.clone();
+                        let path = payload.path.iter().fold(String::new(),|a, b| {
+                            if a.is_empty() {
+                                b.to_string().replace('"', "")
+                            } else {
+                                format!("{}.{}", a, b)
+                            }
+                        });
+
+                        execution_data.dot_set(&path, payload.items.get(0).unwrap());
+                        self.data = Value::Object(execution_data);
+                    } 
                 }
             }
         });
@@ -49,25 +74,28 @@ enum IncrementalPayload {
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct StreamedExecutionResult {
-    hasNext: bool,
+    has_next: bool,
     incremental: Vec<IncrementalPayload>
 }
 
 #[derive(Debug, Deserialize)]
 struct DeferPayload {
     data: Value,
-    path: Vec<String>
+    path: Vec<Value>
 }
 
 #[derive(Debug, Deserialize)]
 struct StreamPayload {
     items: Vec<Value>,
-    path: Vec<String>
+    path: Vec<Value>
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // TODO: delimiter based chunking for multipart vs text/event-stream
+    // TODO: handle normal results as well
     // let mode = "multipart/mixed";
     let mode = "text/event-stream";
     let client = reqwest::Client::new();
@@ -83,7 +111,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .send()
         .await?;
 
-    let mut inital_response = None;
+    let mut final_result = None;
     while let Some(chunk) = resp.chunk().await? {
         let payload = String::from_utf8(chunk.to_vec()).ok().unwrap_or_default();
         if payload.contains("event: next") {
@@ -93,34 +121,60 @@ async fn main() -> Result<(), Box<dyn Error>> {
             match json {
                 Ok(GraphQLResult::ExecutionResult(val)) => {
                     println!("Got initial result {:?}", val);
-                    inital_response = Some(val);
+                    final_result = Some(val);
                 },
                 Ok(GraphQLResult::StreamedExecutionResult(val)) => {
                     println!("Got streamed result {:?}", val);
-                    inital_response = Some(inital_response.clone().unwrap().merge(val).to_owned());
+                    final_result = Some(final_result.clone().unwrap().merge(val).to_owned());
                 }
                 Err(err) => {
                     println!("failed to parese {} {:?}", chunk, err);
                 
                 }
             }
+        } else {
+            final_result = Some(final_result.clone().unwrap().finalize().to_owned());
         }
     }
+    println!("final result: {:?}", &final_result);
 
-    // let body = r#"{
-    //     "query": "query { alphabet @stream }"
-    // }"#;
+    let body = r#"{
+        "query": "query { alphabet @stream }"
+    }"#;
 
-    // let mut resp = client.post("http://localhost:4000/graphql")
-    //     .body(body)
-    //     .header("Accept", mode)
-    //     .header("Content-Type", "application/json")
-    //     .send()
-    //     .await?;
+    let mut resp = client.post("http://localhost:4000/graphql")
+        .body(body)
+        .header("Accept", mode)
+        .header("Content-Type", "application/json")
+        .send()
+        .await?;
 
-    // while let Some(chunk) = resp.chunk().await? {
-    //     println!("Chunk: {:?}", chunk);
-    // }
+    let mut final_result = None;
+    while let Some(chunk) = resp.chunk().await? {
+        let payload = String::from_utf8(chunk.to_vec()).ok().unwrap_or_default();
+        println!("{}", payload);
+        if payload.contains("event: next") {
+            let parts = payload.split("data: ").collect::<Vec<&str>>();
+            let chunk = parts.get(1).unwrap().replace("\n\n", "");
+            let json = serde_json::from_str::<GraphQLResult>(&chunk);
+            match json {
+                Ok(GraphQLResult::ExecutionResult(val)) => {
+                    println!("Got initial result {:?}", val);
+                    final_result = Some(val);
+                },
+                Ok(GraphQLResult::StreamedExecutionResult(val)) => {
+                    println!("Got streamed result {:?}", val);
+                    final_result = Some(final_result.clone().unwrap().merge(val).to_owned());
+                }
+                Err(err) => {
+                    println!("failed to parese {} {:?}", chunk, err);
+                }
+            }
+        } else {
+            final_result = Some(final_result.clone().unwrap().finalize().to_owned());
+        }
+    }
+    println!("final result: {:?}", &final_result);
 
     Ok(())
 }
